99c2f43ad08aae5bba94b0223582becfd21027a8
Karen Gasparyan <mrkaren92@gmail.com>
2023-05-26 10:42:54 +0400

    many to many

--- src/main/java/com/example/listam/entity/Item.java
+++ src/main/java/com/example/listam/entity/Item.java
@@ -1,13 +1,19 @@
 package com.example.listam.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -25,8 +31,14 @@ public class Item {
     @ManyToOne
     private Category category;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
     private String imgName;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "item_hashtag",
+            joinColumns =  @JoinColumn(name = "item_id"),
+            inverseJoinColumns = @JoinColumn(name = "hashtag_id"))
+    List<Hashtag> hashtagList;
 }
